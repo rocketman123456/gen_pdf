892562b70cb96baec3dba5c7048634d86797cdb0
Zhang Yufeng <759094438@qq.com>
2020-11-15 01:28:03 +0800

    Update rename_dir.py

--- rename_dir.py
+++ rename_dir.py
@@ -4,20 +4,7 @@ import re
 import sys
 #设定文件路径
 paths = [
-    #'/Volumes/DATA/Download/(無修正+有修正) 高清动画 第2弹 [漫之学园资源部]',
-    #'/Volumes/DATA/Download/(無修正+有修正) 高清动画 第4弹 [漫之学园资源部]',
-    #'/Volumes/DATA/Download/(無修正+有修正) 高清动画 第5弹 [漫之学园资源部]',
-    #'/Volumes/DATA/Download/(無修正+有修正) 高清动画 第6弹 [漫之学园资源部]',
-    #'/Volumes/DATA/Download/(無修正+有修正) 高清动画 第7弹 [漫之学园资源部]',
-    #'/Volumes/DATA/Download/(無修正+有修正) 高清动画 第8弹 [漫之学园资源部]',
-    #'/Volumes/DATA/Download/(無修正+有修正) 高清动画 第11弹 [漫之学园资源部]',
-    '/Volumes/DATA/Cartoon/(無修正+有修正) 高清动画 第2弹 [漫之学园资源部]',
-    '/Volumes/DATA/Cartoon/(無修正+有修正) 高清动画 第4弹 [漫之学园资源部]',
-    '/Volumes/DATA/Cartoon/(無修正+有修正) 高清动画 第9弹 [漫之学园资源部]',
-    '/Volumes/DATA/Cartoon/(無修正+有修正) 高清动画 第10弹 [漫之学园资源部]',
-    '/Volumes/DATA/Cartoon/(無修正+有修正) 高清动画 第12弹 [漫之学园资源部]',
-    '/Volumes/DATA/Cartoon/(無修正+有修正) 高清动画 第13弹 [漫之学园资源部]',
-    '/Volumes/DATA/Cartoon/(無修正+有修正) 高清动画 第14弹 [漫之学园资源部]',
+    '/path',
 ]
 #对目录下的文件进行遍历
 for path in paths:
@@ -25,8 +12,8 @@ for path in paths:
         #判断是否是文件
         if os.path.isdir(os.path.join(path,category))==True:
             print(category)
-            name1 = '[BanYuner.net整理发布]'
-            name2 = '(15禁アニメ) '
+            name1 = '[]'
+            name2 = '()'
             name3 = ''
             #pattern = re.findall(name,category)
             #_category = category.replace(name1,'')
