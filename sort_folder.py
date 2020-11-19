125dda62c8f336b9b6451b60d776d2c9c29fb633
rocketman123456 <759094438@qq.com>
2020-11-20 02:18:05 +0800

    update sort_folder

--- sort_folder.py
+++ sort_folder.py
@@ -1,10 +1,12 @@
 import os
 import re
 import numpy as np
+import shutil
 
 paths = [
     'G:\\Comic\\201711\\',
 ]
+sort_path = 'G:\\Comic\\sorted\\'
 
 # match [author]
 pattern = r'^\[.+?\]'
@@ -19,13 +21,27 @@ for path in paths:
             if result != None :
                 author = result.group()
                 #print(result.group())
-                new_name = folder.replace(result.group(),'')
+                #new_name = folder.replace(result.group(),'')
                 #print(new_name)
                 if(author not in stored_data):
                     stored_data[author] = []
-                stored_data[author].append(new_name)
+                stored_data[author].append(os.path.join(path, folder))
             else:
                 print(folder)
+for author in stored_data:
+    #print(stored_data[author])
+    new_author_path = os.path.join(sort_path, author)
+    print(new_author_path)
+    isExists = os.path.exists(new_author_path)
+    if not isExists:
+        os.makedirs(new_author_path)
+    for comic in stored_data[author]:
+        old_comic_path = comic
+        new_comic_path = old_comic_path.replace(author,'')
+        #print(old_comic_path)
+        #print(new_comic_path)
+        #os.rename(os.path.join(path,category),os.path.join(path,_category))
+        #shutil.move(new_comic_path, new_author_path)
 #-----------------------------------------------------------------
 np.save('comic.npy', stored_data)
 restored_data = np.load('comic.npy', allow_pickle=True).item()
