a8bfeb60e4f844e50863581dac25ee4e2adb5b76
rocketman123456 <759094438@qq.com>
2020-11-20 13:47:07 +0800

    add unzip function with error

---
+++ sort_file.py
@@ -0,0 +1,84 @@
+import os
+import re
+import numpy as np
+import shutil
+#-----------------------------------------------------------------
+paths = [
+    'G:\\Comic\\201711\\',
+    'G:\\Comic\\201712\\',
+
+    #'G:\\Comic\\201801\\',
+    #'G:\\Comic\\201802\\',
+    #'G:\\Comic\\201803\\',
+    #'G:\\Comic\\201804\\',
+    #'G:\\Comic\\201805\\',
+    #'G:\\Comic\\201806\\',
+    #'G:\\Comic\\201807\\',
+    #'G:\\Comic\\201808\\',
+    #'G:\\Comic\\201809\\',
+    #'G:\\Comic\\201810\\',
+    #'G:\\Comic\\201811\\',
+    #'G:\\Comic\\201812\\',
+
+    #'G:\\Comic\\201901\\',
+    #'G:\\Comic\\201902\\',
+    #'G:\\Comic\\201903\\',
+    #'G:\\Comic\\201904\\',
+    #'G:\\Comic\\201905\\',
+    #'G:\\Comic\\201906\\',
+    #'G:\\Comic\\201907\\',
+    #'G:\\Comic\\201908\\',
+    #'G:\\Comic\\201909\\',
+    #'G:\\Comic\\201910\\',
+    #'G:\\Comic\\201911\\',
+    #'G:\\Comic\\201912\\',
+
+    #'G:\\Comic\\202001\\',
+    ##'G:\\Comic\\202002\\',
+    #'G:\\Comic\\202003\\',
+    #'G:\\Comic\\202004\\',
+    #'G:\\Comic\\202005\\',
+    #'G:\\Comic\\202006\\',
+    #'G:\\Comic\\202007\\',
+    #'G:\\Comic\\202008\\',
+    #'G:\\Comic\\202009\\',
+    #'G:\\Comic\\202010\\',
+]
+#-----------------------------------------------------------------
+# match [author]
+pattern = r'^\[.+?\]'
+#-----------------------------------------------------------------
+# store key-value
+stored_data = {}
+#-----------------------------------------------------------------
+for path in paths:
+    for file_name in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file_name)):
+            result = re.match(pattern=pattern, string=file_name)
+            if result != None :
+                author = result.group()
+                if(author not in stored_data):
+                    stored_data[author] = []
+                stored_data[author].append(os.path.join(path, file_name))
+            else:
+                print(file_name)
+#-----------------------------------------------------------------
+np.save('comic.npy', stored_data)
+restored_data = np.load('comic.npy', allow_pickle=True).item()
+sort_path = 'G:\\Comic\\sorted\\'
+#-----------------------------------------------------------------
+for author in restored_data:
+    new_author_path = os.path.join(sort_path, author)
+    print(new_author_path)
+    isExists = os.path.exists(new_author_path)
+    if not isExists:
+        os.makedirs(new_author_path)
+    for comic in restored_data[author]:
+        old_comic_path = comic
+        new_comic_path = old_comic_path.replace(author + ' ','')
+        #print(old_comic_path)
+        #print(new_comic_path)
+        #os.rename(os.path.join(path,category),os.path.join(path,_category))
+        #shutil.move(new_comic_path, new_author_path)
+#-----------------------------------------------------------------
+print('End')
---
+++ unzip_file.py
@@ -0,0 +1,31 @@
+import zipfile
+import rarfile
+from pathlib import Path
+import os
+
+paths = [
+    'G:\\Comic\\201801\\',
+]
+
+for path in paths:
+    for zip_file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, zip_file)):
+            print(zip_file)
+            f = None
+            if os.path.splitext(zip_file)[1] == '.zip':
+                f = zipfile.ZipFile(os.path.join(path, zip_file), 'r')
+            if os.path.splitext(zip_file)[1] == '.rar':
+                f = rarfile.RarFile(os.path.join(path, zip_file), 'r')
+            namelist = f.namelist()
+            for file_name in f.namelist():
+                extracted_path = Path(f.extract(file_name, path=path))
+                extracted_path.rename(os.path.join(path, file_name).encode('cp437').decode('gbk'))
+            f.close()
+            for name in namelist:
+                if os.path.isdir(os.path.join(path, name)):
+                    os.rmdir(os.path.join(path, name))
+                    break
+print('End')
+
+
+
